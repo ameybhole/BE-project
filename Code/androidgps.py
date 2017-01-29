4ab7cdf82b7ed4c3d5a6c9a24da3069d2687dd62
Amey Bhole <amey.bhole77@gmail.com>
2017-01-29 21:48:19 +0530

    Add files via upload

---
+++ Code/androidgps.py
@@ -0,0 +1,31 @@
+import android
+
+droid = android.Android()
+
+droid.startLocating()
+print "reading GPS ..."
+event = droid.eventWaitFor('location',10000).result
+if event['name'] == "location":
+  try:
+    lat = str(event['data']['gps']['latitude'])
+    lng = str(event['data']['gps']['longitude'])
+  except KeyError:
+    lat = str(event['data']['network']['latitude'])
+    lng = str(event['data']['network']['longitude'])    
+  latlng = 'lat: ' + lat + ' lng: ' + lng
+  print latlng
+
+droid.stopLocating()
+Code Explained
+
+droid.startLocating()
+Start the GPS.
+
+event = droid.eventWaitFor('location',10000).result
+Wait up to 10000ms for location event to occur.
+
+  lat = str(event['data']['gps']['latitude'])
+  lng = str(event['data']['gps']['longitude'])
+Get the lat and lng information.
+
+droid.stopLocating()
